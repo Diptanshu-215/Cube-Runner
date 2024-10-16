80779d6d61e743901277bd80bb12eb3bb1a785f9
Diptanshu-215 <diptanshusaurav215@gmail.com>
2024-10-17 03:28:45 +0530

    Initial commit with .gitignore

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        Debug.Log(collisionInfo.collider.name);
+    }
+}
+
+    
